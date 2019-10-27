f3075ab344ba02e2524c6bbb1c6163f9024fb062
Erika Tsuchiya <tsuchiya.erika@synergy101.jp>
2019-10-28 00:18:43 +0900

    add routing

--- src/components/Addition/index.tsx
+++ src/components/Addition/index.tsx
@@ -1,17 +1,20 @@
 import React, {FC} from 'react'
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {Card} from 'semantic-ui-react';
 
 
 const Home: FC<{}> = () => (
-    <div className="container">
-        <Card>
-        <Card.Content>
-            <Card.Header>Quiz</Card.Header>
-            <Card.Description>1+1=?</Card.Description>
-        </Card.Content>
-        </Card>
-    </div>
+  <div className="container">
+    <Card>
+      <Card.Content>
+        <Card.Header>Quiz</Card.Header>
+        <Card.Description>1+1=?</Card.Description>
+      </Card.Content>
+      <Card.Content>
+        <Link to={`/`}>Home</Link>
+      </Card.Content>
+    </Card>
+  </div>
 );
 
 export default Home;
